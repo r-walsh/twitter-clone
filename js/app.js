48704aac04a6c25e82d9290c4b45267d0c4c5cb0
Ryan Walsh <ryan@thehatrack.net>
2015-08-28 19:16:01 -0600

    First 5 steps complete

---
+++ js/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+
+$('#not-reply').on('click', function(event) {
+	event.stopPropagation();
+
+	$(this).css('height', '5em');
+	$('#tweet-controls').slideDown(100);
+});
+
+$('.tweet-compose').on('click', function(event) {
+	event.stopPropagation();
+
+	$(this).css('height', '5em');
+});
+
+$('html').on('click', function() {
+	$('.tweet-compose').css('height', '2.5em');
+	$('#tweet-controls').slideUp(100);
+});
+
+$('#not-reply').on('keyup', function() {
+	var numOfChars = $(this).val().length;
+	$('#char-count').text(140 - numOfChars);
+	var count = $('#char-count').text();
+
+	if (count < 0) {
+		$('#tweet-submit').attr('disabled', true);		
+	} else if (count > 10) {
+		$('#char-count').css('color', '#999');
+	} else if (count <= 10) {
+		$('#char-count').css('color', 'red');
+	}
+
+});
+
+$('#tweet-submit').on('click', function(e) {
+	e.preventDefault();
+
+
+	var tweet = $('#not-reply').val();
+	$('#not-reply').val('');
+	var lotsOfHTML = $('#reference-tweet').clone();
+
+	$('#stream').prepend(lotsOfHTML);
+	$('#my-tweet').text(tweet)
+	$('#my-tweet-time').text(formatDate());
+	lotsOfHTML.show();
+
+});
+
+function formatDate() {
+	var monthNames = [
+        "Jan", "Feb", "March",
+        "April", "May", "June", "July",
+        "Aug", "Sep", "Oct",
+        "Nov", "Dec"
+    ];
+	var dateStr = '';
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	var day = date.getDate();
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+
+	year = ((year.toString().split('')).splice(2, 2)).join(seperator = '');
+
+	if (hours > 12) {
+		hours -= 12;
+		minutes += ' PM'
+	}
+
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+
+	dateStr = hours + ':' + minutes + ' ' +  '-' + ' ' + day + ' ' + monthNames[month] + ' ' + year;
+	return dateStr;
+}
+
+});
